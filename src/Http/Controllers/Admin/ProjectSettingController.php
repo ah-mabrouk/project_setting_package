922e91d5295737eadd7c916154229788275cda16
Ahmed-Mabrouk <a.mabrouk25@gmail.com>
2024-07-07 15:43:35 +0300

    fix nested routes parameters in project setting admin controller

--- src/Http/Controllers/Admin/ProjectSettingController.php
+++ src/Http/Controllers/Admin/ProjectSettingController.php
@@ -3,7 +3,9 @@
 namespace Mabrouk\ProjectSetting\Http\Controllers\Admin;
 
 use Mabrouk\ProjectSetting\Models\ProjectSetting;
+use Mabrouk\ProjectSetting\Models\ProjectSettingGroup;
 use Mabrouk\ProjectSetting\Http\Controllers\Controller;
+use Mabrouk\ProjectSetting\Models\ProjectSettingSection;
 use Mabrouk\ProjectSetting\Filters\Admin\ProjectSettingFilter;
 use Mabrouk\ProjectSetting\Http\Resources\Admin\ProjectSettingResource;
 use Mabrouk\ProjectSetting\Http\Requests\Admin\ProjectSettingUpdateRequest;
@@ -13,10 +15,12 @@ class ProjectSettingController extends Controller
     /**
      * Display a listing of the resource.
      *
+     * @param  \Mabrouk\ProjectSetting\Models\ProjectSettingGroup  $project_setting_group
+     * @param  \Mabrouk\ProjectSetting\Models\ProjectSettingSection  $project_setting_section
      * @param  \Mabrouk\ProjectSetting\Filters\Admin\ProjectSettingFilter  $filters
      * @return \Illuminate\Http\Response
      */
-    public function index(ProjectSettingFilter $filters)
+    public function index(ProjectSettingGroup $project_setting_group, ProjectSettingSection $project_setting_section, ProjectSettingFilter $filters)
     {
         $paginationLength = pagination_length(ProjectSetting::class);
         $projectSettings = ProjectSetting::filter($filters)->paginate($paginationLength);
@@ -26,10 +30,12 @@ class ProjectSettingController extends Controller
     /**
      * Display the specified resource.
      *
+     * @param  \Mabrouk\ProjectSetting\Models\ProjectSettingGroup  $project_setting_group
+     * @param  \Mabrouk\ProjectSetting\Models\ProjectSettingSection  $project_setting_section
      * @param  \Mabrouk\ProjectSetting\Models\ProjectSetting  $project_setting
      * @return \Illuminate\Http\Response
      */
-    public function show(ProjectSetting $project_setting)
+    public function show(ProjectSettingGroup $project_setting_group, ProjectSettingSection $project_setting_section, ProjectSetting $project_setting)
     {
         return response([
             'project_setting' => new ProjectSettingResource($project_setting),
@@ -40,10 +46,12 @@ class ProjectSettingController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Mabrouk\ProjectSetting\Http\Requests\Admin\ProjectSettingUpdateRequest  $request
+     * @param  \Mabrouk\ProjectSetting\Models\ProjectSettingGroup  $project_setting_group
+     * @param  \Mabrouk\ProjectSetting\Models\ProjectSettingSection  $project_setting_section
      * @param  \Mabrouk\ProjectSetting\Models\ProjectSetting  $project_setting
      * @return \Illuminate\Http\Response
      */
-    public function update(ProjectSettingUpdateRequest $request, ProjectSetting $project_setting)
+    public function update(ProjectSettingUpdateRequest $request, ProjectSettingGroup $project_setting_group, ProjectSettingSection $project_setting_section, ProjectSetting $project_setting)
     {
         $projectSettingGroup = $request->updateProjectSetting();
         return response([
