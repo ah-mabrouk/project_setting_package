aafd302ba34537b8e7e702a158ba5497aa9a1491
Omar20Ashraf <o.ashraf@solutionplus.net>
2025-01-27 11:18:29 +0200

    update displayed field in ProjectSettingUpdateRequest in admin namespace

--- src/resources/lang/en/mabrouk/project_settings/project_settings.php
+++ src/resources/lang/en/mabrouk/project_settings/project_settings.php
@@ -10,5 +10,10 @@ return [
         'custom_validation_rules' => 'project setting custom validation rules',
         'editable' => 'editable',
         'return_to_client' => 'return to client',
+        'displayed' => 'display item in website',
     ],
+
+    'errors' => [
+        'displayed_not_allowed' => 'You can edit this field if the admin have control over it',
+    ],    
 ];
