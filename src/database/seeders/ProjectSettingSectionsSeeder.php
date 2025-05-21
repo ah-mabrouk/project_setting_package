222866d47ea9a61956f2016db5011e9e475b710a
Omar20Ashraf <o.ashraf@solutionplus.net>
2025-05-21 16:50:51 +0300

    wip: add new section seders classes

---
+++ src/database/seeders/ProjectSettingSectionsSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Mabrouk\ProjectSetting\Models\ProjectSettingGroup;
+use Mabrouk\ProjectSetting\Models\ProjectSettingSection;
+
+class ProjectSettingSectionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+
+    public static function seedSections(array $projectSettingSections)
+    {
+        $allProjectSettingGroups = ProjectSettingGroup::all();
+
+        $currentProjectSettingSectionsInTable = ProjectSettingSection::pluck('key')->flatten()->toArray();
+
+        $fillableAttributes = (new ProjectSettingGroup())->getFillable();
+
+        for ($i = 0; $i < \count($projectSettingSections); $i++) {
+            
+            $projectSectionData = $projectSettingSections[$i];
+            $sectionGroup = $allProjectSettingGroups->where('slug', $projectSectionData['group_slug'])->first();
+
+            if (!$sectionGroup || \in_array($projectSectionData['key'], $currentProjectSettingSectionsInTable)) 
+                continue;
+                    
+            $projectSettingSection = $sectionGroup->projectSettingSections()->create(
+                    filteredFillableModelObjectData(actualModelFillable: $fillableAttributes, receivedData: $projectSectionData)
+            );
+
+            addModelTranslation(model: $projectSettingSection, translations: $projectSectionData['translation_data']);
+        }
+    }
+}
